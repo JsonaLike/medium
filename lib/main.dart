03d37acf42480a132e9cc0d84be240a53481245a
yousefmohamed15 <ysfmohamed15@gmail.com>
2021-03-26 18:16:08 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MaterialApp(
-    home: Intro(),
-  ));
+  runApp(Intro());
 }
 
 class Intro extends StatefulWidget {
-
  _FirstLogoAppState createState() => _FirstLogoAppState();
 }
 
 class _FirstLogoAppState extends State<Intro> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    throw UnimplementedError();
+  }
   // TODO: implement createState
 }
 
