20aa0258f013a5d92444a8492e62a6e653654599
yousefmohamed15 <ysfmohamed15@gmail.com>
2021-03-26 16:34:53 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 main() {
   runApp(MaterialApp(
-    home: MyApp(),
+    home: Page1(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
@@ -25,17 +25,9 @@ class MyApp extends StatelessWidget {
 Route _createRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => Page2(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var begin = Offset(0.0, 1.0);
-        var end = Offset.zero;
-        var tween = Tween(begin: begin, end: end);
-        var offsetAnimation = animation.drive(tween);
-
-        return SlideTransition(
-          position: offsetAnimation,
-          child: child,
-        );
-      },
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
   );
 }
 
