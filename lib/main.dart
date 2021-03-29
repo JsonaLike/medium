597baca02bd7b4bc1449fbf074b606a09f1ab38b
yousefmohamed15 <ysfmohamed15@gmail.com>
2021-03-29 21:25:59 +0300

    added width&height factors

--- lib/main.dart
+++ lib/main.dart
@@ -63,9 +63,12 @@ class _MyHomePageState extends State<MyHomePage>
       return Scaffold(
         body: Container(
             color: background.evaluate(AlwaysStoppedAnimation(_controller.value)),
-            child: Center(child: FractionallySizedBox(
-            child:Image(image: AssetImage('graphics/logo.png')))
-
+            child: Center(
+                child: FractionallySizedBox(
+                    child:Image(image: AssetImage('graphics/logo.png')),
+                      widthFactor: 0.3,
+                      heightFactor: 0.1,
+            )
       )
         ),
       );
